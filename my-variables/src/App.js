bbdcd5df540252812b672273fc46fc133879cf50
pranavbaishnair2025 <pranav.baishnair2025@vitstudent.ac.in>
2026-07-03 18:52:54 +0530

    week6

--- my-variables/src/App.js
+++ my-variables/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Card from "./menucard";
 function App(){
-
+  var cafename="Irish"
+  function greet(){
+    alert("Welcome user")
+  }
   return(
     <div>
-      <h2>Welcome to React</h2>
+      <h2>Welcome to {cafename}</h2>
       <h2>Menu</h2>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <button onClick={greet}>Greeting</button>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
 
   )
--- my-variables/src/menucard.js
+++ my-variables/src/menucard.js
@@ -3,8 +3,8 @@ import './card.css'
 function Card(){
 
   return(
-    <div classname="menu">
-      <div classname="product">
+    <div className="menu">
+      <div className="product">
         <img src="https://images.pexels.com/photos/6068717/pexels-photo-6068717.jpeg" alt="Pizza" height="200" width="200"></img>
         <h2>Pizza</h2>
         <p>Rs. 500</p>
